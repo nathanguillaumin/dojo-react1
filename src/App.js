b33e637ba6feb30572d2060b86ff16363115d1bb
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-04-21 12:47:15 +0200

    dojo done

--- src/App.js
+++ src/App.js
@@ -7,12 +7,69 @@ const initialProductList = [
   { id: 3, name: 'produit 3', price: 20, quantity: 5 }
 ];
 
-function App () {
-  return (
-    <div className='App'>
-      <h1>Ma commande</h1>
-    </div>
-  );
+/*
+Abdel-rahim
+Camille
+Ikram
+Nathan
+Armin
+*/
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      products: initialProductList
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange (event, productID) {
+    console.log(productID);
+    console.log( event.target.value);
+    const newProducts = this.state.products.map(product => {
+      if (product.id === productID) {
+        return { ...product, quantity: event.target.value };
+      } else {
+        return product;
+      }
+    });
+    console.log(newProducts);
+    this.setState({ products: newProducts });
+  }
+
+  render () {
+    const totaux = this.state.products.map(product => (product.price * product.quantity));
+    return (
+      <div className='App'>
+        <h1>Ma commande</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Produit</th>
+              <th>Prix unitaire</th>
+              <th>Quantité</th>
+              <th>Prix total</th>
+            </tr>
+          </thead>
+
+          <tbody>
+
+            {this.state.products.map(produit =>
+              <tr key={produit.id}>
+                <td key={produit.name}>{produit.name}</td>
+                <td key={produit.price}>{produit.price}</td>
+                <td><input type='number' min='0' onChange={(event) => {this.handleChange(event, produit.id)}} value={produit.quantity} /></td>
+                <td key={produit.id}>{produit.price * produit.quantity}</td>
+              </tr>)}
+          </tbody>
+        </table>
+
+        <h3>Montant de la commande: {totaux.reduce((total, currentValue) => total + currentValue)}€</h3>
+
+      </div>
+    );
+  }
 }
 
 export default App;
